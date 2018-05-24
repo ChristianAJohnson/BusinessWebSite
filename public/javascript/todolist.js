19267aa951ddb470ae71518fc31f691b61ca92a0
AaronHale1989 <halea02130@gmail.com>
2018-05-24 14:56:19 -0400

    slideshow finished

--- public/javascript/todolist.js
+++ public/javascript/todolist.js
@@ -2,10 +2,10 @@
 var toDoList = document.getElementById("toDoList");
 var movieBox = document.getElementsByClassName("movieBox");
 var listWrapper = document.getElementById("listWrapper");
+var slideshowContainer = document.getElementById("slideshowContainer");
+var imageSlides = document.getElementsByClassName("imageslide");
 
 var movieArray = ["blackmirror","bladerunner","fifthelement","fightclub","ghostbusters","grr","it","jaws","madmax","pulpfiction","romeomustdie","silenceofthelambs","thelastjedi","themartian","titanic"];
-
-var jpegArray = ["blackmirror.jpeg","bladerunner.jpeg","fifthelement.jpeg","fightclub.jpeg","ghostbusters.jpeg","grr.jpeg","it.jpeg","jaws.jpeg","madmax.jpeg","pulpfiction.jpeg","romeomustdie.jpeg","silenceofthelambs.jpeg","thelastjedi.jpeg","themartian.jpeg","titanic.jpeg"]
 
 var movieImageArray = []
 
@@ -19,25 +19,55 @@ for(let i=0;i<movieArray.length;i++){
 	movieBox.style.height = "30px";
 	movieBox.style.width = "125px";
 	movieBox.style.fontFamily = "courier";
-	movieBox.style.backgroundColor = "#42b6f4";
+	movieBox.style.backgroundColor = "#A0E838";
 	movieBox.style.margin = "5px";
 	movieBox.style.borderRadius = "30px"
 	movieBox.style.textAlign = "center";
+	movieBox.style.textTransform = "capitalize";
+	movieBox.style.fontFamily = "'Lato', sans-serif";
 	movieBox.innerHTML = movieArray[i];
 	movieListWrapper.appendChild(movieBox);
 	movieBox.addEventListener("click",function(e){
-		var imageBox = document.createElement("div");
-		toDoList.appendChild(imageBox)
-		imageBox.style.height = "150px";
-		imageBox.style.width = "248px";
-		imageBox.style.backgroundImage = "url('images/" + movieArray[i] + ".jpeg')";
-		imageBox.style.backgroundSize = "100%"
 		movieImageArray.push(movieArray[i]);
 		console.log(movieImageArray)
-
-
+		var n = movieImageArray.indexOf(movieArray[i])
+		imageSlides[n].setAttribute("src","images/" + movieImageArray[n]+".jpeg")
 	})
 }
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
+
+
+
+
+
 
 
 
