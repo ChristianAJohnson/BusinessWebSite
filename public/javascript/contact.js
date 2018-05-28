1ced5d0bad1b6faf38d2f108afabd13b542bf822
ChristianAJohnson <chrisgochris@gmail.com>
2018-05-28 16:57:26 -0400

    updating gallery and contact

---
+++ public/javascript/contact.js
@@ -0,0 +1,24 @@
+
+var submitName = document.getElementById("submitName")
+var comment = document.getElementById("comment")
+var submitButton = document.getElementById("submitButton")
+var commentText = document.getElementById("commentText")
+
+function CommentData()
+{
+  
+  	var hintText = document.createElement("div")
+	// hintText.style.width = "auto"
+	// hintText.style.border = "2px solid blue"
+	// hintText.className = "hintText"
+	// hintText.innerHTML = name.value
+	hintText.innerHTML = "<br>" +"Name: " + submitName.value + "<br>" + "Comment: " + comment.value
+
+	commentText.appendChild(hintText)
+}
+
+submitButton.addEventListener("click",function()
+{
+	CommentData()
+
+});
--- public/javascript/gallery.js
+++ public/javascript/gallery.js
@@ -1,18 +1,25 @@
 
-
 var movieArray = ["blackmirror","bladerunner","fifthelement","fightclub","ghostbusters","grr","it","jaws","madmax","pulpfiction","romeomustdie","silenceofthelambs","thelastjedi","themartian","titanic"];
 
 var newMovies = document.getElementById("newMovies");
 var newMoviesleft = document.getElementById("newMoviesleft")
 var newMoviesright = document.getElementById("newMoviesright")
-var newMovieNumber = 0
+var newMovieNumber = 0;
 
 
 var scifi = document.getElementById("scifi");
+var newscifileft  = document.getElementById("newscifileft");
+var newscifiright = document.getElementById("newscifiright");
+var scifiNumber = 1;
+
+
 var other = document.getElementById("other");
+var newotherleft = document.getElementById("newotherleft");
+var newotherright = document.getElementById("newotherright");
+var otherNumber= 7;
 
-
-
+var picWidth = "180px"
+var picHeight = "275px"
 
 
 function ShowAllMovies()
@@ -23,7 +30,7 @@ function ShowAllMovies()
 		var picture = document.createElement("div")
 		picture.style.width = "200px"
 		picture.style.height = "170px	"
-		picture.style.border = "2px solid blue"
+		// picture.style.border = "2px solid blue"
 		// picture.style.marginTop = "10px"
 		picture.style.display=  "inline-block"
 		picture.style.backgroundImage = "url(../images/"+movieArray[i]+".jpeg)"
@@ -35,9 +42,10 @@ function ShowAllMovies()
 function ShowNewMovies(image)
 {
 	var picture = document.createElement("div")
-	picture.style.width = "200px"
-	picture.style.height = "170px	"
-	picture.style.border = "2px solid blue"
+	picture.setAttribute('id', 'test'+ newMovieNumber)
+	picture.style.width = picWidth
+	picture.style.height = picHeight
+	// picture.style.border = "2px solid blue"
 	// picture.style.marginTop = "10px"
 	picture.style.display=  "inline-block"
 	picture.style.backgroundImage = "url(../images/"+image+".jpeg)"
@@ -47,9 +55,9 @@ function ShowNewMovies(image)
 function ShowScifiMovies(image)
 {
 	var picture = document.createElement("div")
-	picture.style.width = "200px"
-	picture.style.height = "170px"
-	picture.style.border = "2px solid blue"
+	picture.style.width = picWidth
+	picture.style.height = picHeight
+	// picture.style.border = "2px solid blue"
 	// picture.style.marginTop = "10px"
 	picture.style.display=  "inline-block"
 	picture.style.backgroundImage = "url(../images/"+image+".jpeg)"
@@ -59,9 +67,9 @@ function ShowScifiMovies(image)
 function ShowOtherMovies(image)
 {
 	var picture = document.createElement("div")
-	picture.style.width = "200px"
-	picture.style.height = "170px	"
-	picture.style.border = "2px solid blue"
+	picture.style.width = picWidth
+	picture.style.height = picHeight
+	// picture.style.border = "2px solid blue"
 	// picture.style.marginTop = "10px"
 	picture.style.display=  "inline-block"
 	picture.style.backgroundImage = "url(../images/"+image+".jpeg)"
@@ -70,8 +78,8 @@ function ShowOtherMovies(image)
 
 
 ShowNewMovies(movieArray[newMovieNumber])
-ShowScifiMovies(movieArray[2])
-ShowOtherMovies(movieArray[7])
+ShowScifiMovies(movieArray[scifiNumber])
+ShowOtherMovies(movieArray[otherNumber])
 
 newMoviesleft.addEventListener("click", function(){
 	newMovieNumber = newMovieNumber - 1
@@ -82,6 +90,32 @@ newMoviesright.addEventListener("click", function(){
 	newMovieNumber = newMovieNumber + 1
 	ShowNewMovies(movieArray[newMovieNumber])
 })
+
+
+newscifileft.addEventListener("click", function(){
+	scifiNumber = scifiNumber - 1
+	ShowScifiMovies(movieArray[scifiNumber])
+})
+
+newscifiright.addEventListener("click", function(){
+	scifiNumber = scifiNumber + 1
+	ShowScifiMovies(movieArray[scifiNumber])
+})
+
+
+newotherleft.addEventListener("click", function(){
+	otherNumber = otherNumber - 1
+	ShowOtherMovies(movieArray[otherNumber])
+})
+
+newotherright.addEventListener("click", function(){
+	otherNumber = otherNumber + 1
+	ShowOtherMovies(movieArray[otherNumber])
+})
+
+
+
+
 
 // newMoviesright.addEventListener("click", function(){
 // 	newMovieNumber = newMovieNumber + 1
